9edad16bb0840112852e2ea67661c38749de6773
Quentin ADAM <quentin.adam@waxzce.org>
2012-08-04 12:37:40 +0200

    first java commit

---
+++ instavan-server/src/main/java/us/companycamp/instavan/rest/ApplicationConfig.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package us.companycamp.instavan.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author waxzce
+ */
+@ApplicationPath("/resources/")
+public class ApplicationConfig extends Application {
+    
+}
---
+++ instavan-server/src/main/java/us/companycamp/instavan/rest/GenericResource.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package us.companycamp.instavan.rest;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author waxzce
+ */
+@Path("photos")
+public class GenericResource {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of GenericResource
+     */
+    public GenericResource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of us.companycamp.instavan.rest.GenericResource
+     * @return an instance of javax.ws.rs.core.Response
+     */
+    @GET
+    @Produces("application/json")
+    public Response getJson() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of GenericResource
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/json")
+    public void putJson(Response content) {
+    }
+}
